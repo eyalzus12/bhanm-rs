830a145635f4a51493ecb922b6069b95f6e29184
EyalZusiman <eyal.zusiman@gmail.com>
2025-06-10 22:35:39 +0300

    add export documentation

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,13 @@
 //! A Rust library for reading and writing Brawlhalla's anm files.
 //!
 //! # Organization
+//!
+//! This library exports the following:
+//! * `AnmBone`: A sprite to be positioned in the world.
+//! * `AnmFrame`: A single frame of animation.
+//! * `AnmAnimation`: A complete animation.
+//! * `AnmClass`: A collection of animations, indexed by their name.
+//! * `AnmFile`: A collection of animation classes.
 
 mod anm_objects;
 
